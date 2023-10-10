d50f9097a9ff4741053bafc432d6540266d09c35
a-honey <jungahyeon0512@khu.ac.kr>
2023-10-11 02:20:44 +0900

    Docs: useQueries 관련 설명 주석

--- src/hooks/queries/useGetInitCalling.ts
+++ src/hooks/queries/useGetInitCalling.ts
@@ -3,6 +3,7 @@ import { getTodo, getUserInfo } from "../../api/apis";
 
 export function useGetInitCalling({ id }: { id: number }) {
   return useQueries({
+    // 여러 개의 쿼리를 한 번에 실행하고 관리(병렬 실행)
     queries: [
       { queryKey: ["useGetTodo", id], queryFn: () => getTodo({ todoId: id }) },
       {
@@ -16,7 +17,7 @@ export function useGetInitCalling({ id }: { id: number }) {
 export function useGetMapQueries({ dependArray }: { dependArray: number[] }) {
   return useQueries({
     queries: dependArray.map((i) => ({
-      queryKey: ["useGetUserInfo", i],
+      queryKey: ["useGetUserInfo", i], // userId를 배열로 받아서 정보를 저장
       queryFn: () => getUserInfo({ userId: i }),
       enabled: !!dependArray,
     })),
