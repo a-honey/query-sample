e1a374511f67779e3f27c4434f32198bcbb0f269
a-honey <jungahyeon0512@khu.ac.kr>
2023-10-11 02:06:56 +0900

    Docs: queryClient 관련 설명 주석

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,20 @@ import { RecoilRoot } from "recoil";
 import Router from "./Router";
 
 const queryClient = new QueryClient({
+  //  API 또는 다른 소스에서 데이터를 관리하고 가져오는 데 사용
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-      retry: false,
+      refetchOnWindowFocus: false, // 창이 다시 포커스되었을때, 쿼리 자동으로 가져오는지
+      refetchOnMount: false, // 컴포넌트 마운트될 때, 쿼리를 자동으로 다시 가져오는지
+      retry: false, // 실패한 쿼리 자동으로 재시도하는지  // 일정 횟수 지정 가능
     },
   },
   queryCache: new QueryCache({
+    // 쿼리 결과를 캐시하고 쿼리 관련 이벤트를 처리하는 역할
     onError: (error, query) => {
+      // 쿼리 중 오류가 발생했을 때 어떻게 처리할지 설정
       if (query.meta.errorMessage) {
+        // 쿼리에 meta.errorMessage 속성있는지 확인 후 출력
         console.log(query.meta.errorMessage);
       }
     },
@@ -29,6 +33,7 @@ function App() {
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <Router />
+        {/* react-query 개발자 도구 */}
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </RecoilRoot>
