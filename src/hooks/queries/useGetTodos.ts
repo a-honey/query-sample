ad6059d4e9f21d188ded622b3d5026966294a2da
a-honey <jungahyeon0512@khu.ac.kr>
2023-10-11 02:32:02 +0900

    Docs: useInfiniteQuery 설명 주석

--- src/hooks/queries/useGetTodos.ts
+++ src/hooks/queries/useGetTodos.ts
@@ -4,10 +4,9 @@ import { getTodo } from "../../api/apis";
 export default function useGetTodos() {
   return useInfiniteQuery(
     ["useGetTodosInfinite"],
-    ({ pageParam = 1 }) => getTodo({ todoId: pageParam }),
+    ({ pageParam = 1 }) => getTodo({ todoId: pageParam }), // 초기 페이지 번호 1, 데이터를 가져옴
     {
-      getNextPageParam: (lastPage) => lastPage.id < 10 && lastPage.id + 1,
-      getPreviousPageParam: (firstPage) => firstPage.id > 0 && firstPage.id - 1,
+      getNextPageParam: (lastPage) => lastPage.id < 10 && lastPage.id + 1, // useGetTodosInfinite의 마지막 페이지의 id가 10미만(마지막페이지가 아닌 경우)인 경우 다음 페이지를 가져옴
       select: (data) => {
         return data;
       },
