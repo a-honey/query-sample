a905ef79b0387a1ea10043acdd1e6cd5eb97cdc5
jeonghyun <ptr1129@paycrux.co.kr>
2023-10-10 22:15:42 +0900

    Fix : 이해를 위한 코드 삭제

--- src/pages/GetQueriesPage.tsx
+++ src/pages/GetQueriesPage.tsx
@@ -10,8 +10,11 @@ import useGetUserInfo from "../hooks/queries/useGetUserInfo";
 export default function GetQueriesPage() {
   const [count] = useState(1);
 
+  // getTodo, getUserInfo가 비동기적으로 선언되는 일반적인 용례입니다.
   const { data: todoData } = useGetTodo({ todoId: count });
   const { data: userInfoData } = useGetUserInfo({ userId: count });
+
+  // 아래는 getTodo, getUserInfo를 useQueries로 묶어내어 각각의 useQuery로서 기능하도록 해둔 용례입니다.
 
   // const [
   //   {
@@ -29,6 +32,8 @@ export default function GetQueriesPage() {
   //     isError: res2Error,
   //   },
   // ] = useGetInitCalling({ id: 1 });
+
+  // 아래는 useQueries를 활용하는 또 다른 방식의 예시입니다. 실제 코드를 열어보시면 됩니다.
 
   //   const res = useGetMapQueries({ dependArray: [1, 2, 3, 4, 5] });
 
